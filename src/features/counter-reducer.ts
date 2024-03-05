f238637bf68601f92b84490fc9114e76bb875f7f
Simone Taeggi <startae14@gmail.com>
2024-03-05 22:21:31 +0100

    solution createReducer and use of the new reducer

--- src/features/counter-reducer.ts
+++ src/features/counter-reducer.ts
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 type CounterState = {
   count: number;
@@ -6,24 +6,38 @@ type CounterState = {
 
 // createAction
 // increment
-const increment = createAction('INCREMENT', (amount: number) => ({
+export const increment = createAction('INCREMENT', (amount: number) => ({
   payload: amount,
 }));
 
 // const incrementAction = increment(3);
 
 // decrement
-const decrement = createAction('DECREMENT', (amount: number) => ({
+export const decrement = createAction('DECREMENT', (amount: number) => ({
   payload: amount,
 }));
 
 // reset
-const reset = createAction('RESET');
+export const reset = createAction('RESET');
 
 type CounterAction =
   | ReturnType<typeof increment>
   | ReturnType<typeof decrement>
   | ReturnType<typeof reset>;
+
+export const counterReducer = createReducer({ count: 0 }, (builder) => {
+  builder.addCase(increment, (state, action) => {
+    state.count += action.payload;
+  });
+
+  builder.addCase(decrement, (state, action) => {
+    state.count -= action.payload;
+  });
+
+  builder.addCase(reset, (state) => {
+    state.count = 0;
+  });
+});
 
 export const reducer = (state: CounterState, action: CounterAction) => {
   if (action.type === increment.type) {
